50fa382043790572ebaf91bbe8562985c42b98e6
”SulingXu“ <xusuling2020@gmail.com>
2020-12-26 09:20:34 +0100

    Initial Commit

---
+++ PlantWatering/PlantWateringApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantWateringApp.swift
+//  PlantWatering
+//
+//  Created by Suling Xu on 12/26/20.
+//
+
+import SwiftUI
+
+@main
+struct PlantWateringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
