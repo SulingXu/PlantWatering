110ac1ef4d91bc700c02cb7bb0b49ca8f5c87ff8
”SulingXu“ <xusuling2020@gmail.com>
2020-12-29 10:30:23 +0100

    Declare a new model type that conforms to the ObservableObject protocol from the Combine framework and add @Published attribute to the landmark array after moving it into the model.

--- PlantWatering/Models/ModelData.swift
+++ PlantWatering/Models/ModelData.swift
@@ -7,13 +7,16 @@
 
 import Foundation
 import SwiftUI
+import Combine
 
-var plants: [Plant] = [
-    Plant(name: "Plant 1", lastWateringTime: Date(), schedule: 10, nextWateringTime: "1 day later", hasWatered: false, image: Image("Spathiphyllum")),
-    Plant(name: "Plant 2", lastWateringTime: Date(), schedule: 10, nextWateringTime: "2 day later", hasWatered: false, image: Image("Spathiphyllum")),
-    Plant(name: "Plant 3", lastWateringTime: Date(), schedule: 10, nextWateringTime: "3 day later", hasWatered: false, image: Image("Spathiphyllum")),
-    Plant(name: "Plant 4", lastWateringTime: Date(), schedule: 10, nextWateringTime: "4 day later", hasWatered: false, image: Image("Spathiphyllum")),
-    Plant(name: "Plant 5", lastWateringTime: Date(), schedule: 10, nextWateringTime: "5 day later", hasWatered: false, image: Image("Spathiphyllum")),
-    Plant(name: "Plant 6", lastWateringTime: Date(), schedule: 10, nextWateringTime: "6 day later", hasWatered: false, image: Image("Spathiphyllum")),
-    Plant(name: "Plant 7", lastWateringTime: Date(), schedule: 10, nextWateringTime: "7 day later", hasWatered: false, image: Image("Spathiphyllum")),
-]
+final class ModelData: ObservableObject {
+    @Published var plants: [Plant] = [
+        Plant(name: "Spathiphyllum", lastWateringTime: Date(), schedule: 7, nextWateringTime: "7 days later", hasWatered: true, image: Image("Spathiphyllum"), description: "It lives best in shade and needs little sunlight to thrive, and is watered approximately once a week. The soil is best left moist but only needs watering if the soil is dry."),
+        Plant(name: "Plant 2", lastWateringTime: Date(), schedule: 10, nextWateringTime: "2 days later", hasWatered: true, image: Image("Spathiphyllum"), description: "It lives best in shade and needs little sunlight to thrive, and is watered approximately once a week. The soil is best left moist but only needs watering if the soil is dry."),
+        Plant(name: "Plant 3", lastWateringTime: Date(), schedule: 10, nextWateringTime: "3 days later", hasWatered: false, image: Image("Spathiphyllum"), description: "It lives best in shade and needs little sunlight to thrive, and is watered approximately once a week. The soil is best left moist but only needs watering if the soil is dry."),
+        Plant(name: "Plant 4", lastWateringTime: Date(), schedule: 10, nextWateringTime: "4 days later", hasWatered: true, image: Image("Spathiphyllum"), description: "It lives best in shade and needs little sunlight to thrive, and is watered approximately once a week. The soil is best left moist but only needs watering if the soil is dry."),
+        Plant(name: "Plant 5", lastWateringTime: Date(), schedule: 10, nextWateringTime: "5 days later", hasWatered: false, image: Image("Spathiphyllum"), description: "It lives best in shade and needs little sunlight to thrive, and is watered approximately once a week. The soil is best left moist but only needs watering if the soil is dry."),
+        Plant(name: "Plant 6", lastWateringTime: Date(), schedule: 10, nextWateringTime: "6 days later", hasWatered: false, image: Image("Spathiphyllum"), description: "It lives best in shade and needs little sunlight to thrive, and is watered approximately once a week. The soil is best left moist but only needs watering if the soil is dry."),
+        Plant(name: "Plant 7", lastWateringTime: Date(), schedule: 10, nextWateringTime: "7 days later", hasWatered: false, image: Image("Spathiphyllum"), description: "It lives best in shade and needs little sunlight to thrive, and is watered approximately once a week. The soil is best left moist but only needs watering if the soil is dry.")
+    ]
+}
