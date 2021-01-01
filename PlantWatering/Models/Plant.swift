440f1ffdfbb9bac74681a7908c4d29c379e4d641
”SulingXu“ <xusuling2020@gmail.com>
2021-01-01 18:07:19 +0100

    add a computed variable diffInDays to indicate how many days left for next time watering

--- PlantWatering/Models/Plant.swift
+++ PlantWatering/Models/Plant.swift
@@ -24,7 +24,23 @@ struct Plant: Identifiable {
     let schedule: Int
     var hasWatered: Bool
     let image: Image
-    
     let description: String
     
+    var diffInDays: String {
+        let calendar = Calendar.current
+
+        // Replace the hour (time) of both dates with 00:00
+        let date1 = calendar.startOfDay(for: Date())
+        let date2 = calendar.startOfDay(for: nextWateringTime)
+
+        let components = calendar.dateComponents([.day], from: date1, to: date2)
+        
+        if (nextWateringTime <= Date()){
+            return "Today"
+        }else if components.day! == 1 {
+            return "1 day later"
+        } else {
+            return "\(components.day!) days later"
+        }
+    }
 }
